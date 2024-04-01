2c87abcdc4d437295a5a6f77785f22bc030ff3e3
ctsMukesh <156057428+ctsMukesh@users.noreply.github.com>
2024-04-01 22:28:22 +0530

    UI Changes

---
+++ GoContacts.UnitTests/ContactsController/ContactsControllerTest.cs
@@ -0,0 +1,16 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+
+
+namespace GoContacts.UnitTests.ContactsController
+{
+    [TestFixture]
+    public class ContactsControllerTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GoContacts.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoContacts.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cognizant")]
+[assembly: AssemblyProduct("GoContacts.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Cognizant 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3340406-e228-45c7-a832-5e29680bf54e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
